8ad48fff3109cf0759637b27672b5e3ee08fe621
Paul Ayamah <payamahp@gmail.com>
2023-09-07 19:11:45 -0400

    latest

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,31 @@
 console.log('hello world');
+
+document.addEventListener("DOMContentLoaded", function () {
+    const clickButton = document.getElementById("clickButton");
+    const resetButton = document.getElementById("resetButton");
+    const countDisplay = document.getElementById("count");
+    const circle = document.querySelector(".circle");
+
+    let count = 0;
+    let fontSize = 24;
+
+    clickButton.addEventListener("click", function () {
+        count++;
+        countDisplay.textContent = count;
+        fontSize += 2; 
+        countDisplay.style.fontSize = fontSize + "px";
+
+        const newSize = 100 + count * 10; 
+        circle.style.width = newSize + "px";
+        circle.style.height = newSize + "px";
+    });
+
+    resetButton.addEventListener("click", function () {
+        count = 0;
+        fontSize = 24;
+        countDisplay.textContent = count;
+        countDisplay.style.fontSize = fontSize + "px";
+        circle.style.width = "100px"; 
+        circle.style.height = "100px";
+    });
+});
