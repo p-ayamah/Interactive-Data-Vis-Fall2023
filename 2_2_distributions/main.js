6c303da3421e6c904da00bfa7b6e6861c4a13394
Paul Ayamah <payamahp@gmail.com>
2023-10-06 21:08:35 -0400

    new update

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,97 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 40, bottom: 60, left: 60, right: 40 };
 
 /* LOAD DATA */
-d3.csv("../data/MoMA_distributions.csv", d3.autoType)
-  .then(data => {
-    console.log(data)
+d3.csv("../data/MoMA_distributions.csv", d3.autoType).then(data => {
+  console.log(data);
 
-    /* SCALES */
+  //THE SVG CONTAINER
+  const svg = d3
+    .select("#container")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width);
+
+  // SCALES
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, d => d['Width (cm)']))
+    .range([margin.left, width - margin.right]);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, d => d['Length (cm)']))
+    .range([height - margin.bottom, margin.top]);
+
+  const radius = d3
+    .scaleLinear()
+    .domain(d3.extent(data, d => d['Artist Lifespan']))
+    .range([5, 30]);
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  // BINDING DOM ELEMENTS TO DATA
+  svg
+    .selectAll("circle.artifact")
+    .data(data)
+    .join("circle")
+    .attr("class", "artifact")
+    .attr("r", d => radius(d['Artist Lifespan']))
+    .attr("fill", "blue")
+    .attr("transform", d => `translate(${xScale(d['Width (cm)'])},${yScale(d['Length (cm)'])})`);
+
+   //LABELING THE CIRCLES (PLOT ELEMENTS) 
+  svg
+    .selectAll("text.label")
+    .data(data)
+    .join("text")
+    .attr("class", "label")
+    .attr("x", d => xScale(d['Width (cm)']) + 5)
+    .attr("y", d => yScale(d['Length (cm)']) - 5)
+    .text("ls")
+    .attr("font-size", "10px");
+
+    //THE X AXIS
+  svg
+    .append("g")
+    .call(selection => selection.call(xAxis))
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
     
-    /* HTML ELEMENTS */
-    
-  });
+//THE Y AXIS
+  svg
+    .append("g")
+    .call(selection => selection.call(yAxis))
+    .attr("transform", `translate(${margin.left}, 0)`)
+
+    //X-Axis Title
+    svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", height+40-margin.bottom)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "15px")
+    .text("Width of Artifact (cm)");
+
+    //Y-Axis Title
+    svg
+    .append("text")
+    .attr("x", -height / 2)
+    .attr("y", margin.left / 4)
+    .attr("transform", "rotate(-90)")
+    .style("text-anchor", "middle")
+    .text("Length of Artifact (cm)");
+
+
+
+  // Main title
+  svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", margin.top - 18)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "30px")
+    .text("Relationship between Length and Width of MoMA Artifacts");
+});
--- 3_4_geographic/main.js
+++ 3_4_geographic/main.js
@@ -1,42 +1,131 @@
-/**
- * CONSTANTS AND GLOBALS
- * */
+/* CONSTANTS AND GLOBALS */
+const width = window.innerWidth * 0.9,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
-
-
-/**
-* APPLICATION STATE
-* */
-let state = {
-
-};
+// Define values for the legend
+const legendValues = [100, 1000, 2000, 3000, 5000]; // Adjust these values based on your data
 
 /**
-* LOAD DATA
-* Using a Promise.all([]), we can load more than one dataset at a time
-* */
+ * LOAD DATA
+ * Using a Promise.all([]), we can load more than one dataset at a time
+ */
 Promise.all([
-  d3.json("../data/usState.json")
-]).then(([geojson]) => {
-  state.geojson = geojson;
-  // console.log("state: ", state);
-  init();
+  d3.json("../data/world.json"),
+  d3.csv("../data/MoMA_nationalities.csv", d3.autoType),
+]).then(([geojson, nationalities]) => {
+  // Create a Set of countries from the CSV file
+  const countriesFromCSV = new Set(nationalities.map(d => d.Country));
+
+  // Get the maximum value from the "Count" column
+  const maxCount = d3.max(nationalities, d => d.Count);
+
+  // Create a scale for circle size based on the "Count" column
+  const circleSizeScale = d3
+    .scaleLinear()
+    .domain([0, maxCount])
+    .range([5, 30]); // Adjust the range for the desired circle size range
+
+  const svg = d3
+    .select("#container")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .style("background-color", "aliceblue");
+
+  // Add a title to the graph
+  svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", margin.top)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "20px")
+    .text("Choropleth Map with Circle Sizes");
+
+  // SPECIFY PROJECTION
+  const projection = d3.geoNaturalEarth1().fitSize([width, height], geojson);
+
+  // DEFINE PATH FUNCTION
+  const pathGen = d3.geoPath().projection(projection);
+
+  // APPEND GEOJSON PATH for all features
+  const countries = svg
+    .selectAll("path.countries")
+    .data(geojson.features)
+    .join("path")
+    .attr("class", "countries")
+    .attr("d", d => pathGen(d))
+    .attr("stroke", "black")
+    .attr("fill", d => {
+      if (d.properties.name === "United States of America") {
+        // Color the United States differently and reduce its size by 30%
+        return "#005427";
+      } else if (countriesFromCSV.has(d.properties.name)) {
+        // Fill other shaded countries with green at alpha 0.6
+        return "rgba(0, 128, 0, 0.6)";
+      } else {
+        return "transparent";
+      }
+    });
+
+  // Append circles to shaded countries based on the "Count" column
+  const circles = svg
+    .selectAll("circle")
+    .data(nationalities.filter(d => countriesFromCSV.has(d.Country)))
+    .enter()
+    .append("circle")
+    .attr("cx", d => {
+      const countryFeature = geojson.features.find(
+        country => country.properties.name === d.Country
+      );
+      const centroid = pathGen.centroid(countryFeature);
+      return centroid[0]; // X-coordinate based on centroid
+    })
+    .attr("cy", d => {
+      const countryFeature = geojson.features.find(
+        country => country.properties.name === d.Country
+      );
+      const centroid = pathGen.centroid(countryFeature);
+      return centroid[1]; // Y-coordinate based on centroid
+    })
+    .attr("r", d => {
+      if (d.Country === "United States of America") {
+        // Reduce the size of the circle for the United States by 30%
+        return circleSizeScale(d.Count);
+      } else {
+        return circleSizeScale(d.Count);
+      }
+    }) // Size of circle based on "Count" column
+    .attr("fill", "green")
+    .attr("fill-opacity", 0.6) // Alpha value for fill
+    .attr("stroke", "black");
+
+  // Create a legend
+  const legend = svg
+    .selectAll(".legend")
+    .data(legendValues)
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => `translate(${width - 100}, ${margin.top + i * 50})`);
+
+  // Add legend circles
+  legend
+    .append("circle")
+    .attr("cx", 10)
+    .attr("cy", 10)
+    .attr("r", d => circleSizeScale(d))
+    .attr("fill", "green")
+    .attr("fill-opacity", 0.6)
+    .attr("stroke", "black");
+
+  // Add legend labels
+  legend
+    .append("text")
+    .attr("x", 40)
+    .attr("y", 10)
+    .attr("dy", "0.35em")
+    .attr("font-size", "12px")
+    .text(d => d);
+
 });
-
-/**
-* INITIALIZING FUNCTION
-* this will be run *one time* when the data finishes loading in
-* */
-function init() {
- 
-  draw(); // calls the draw function
-}
-
-/**
-* DRAW FUNCTION
-* we call this every time there is an update to the data/state
-* */
-function draw() {
- 
-
-}
